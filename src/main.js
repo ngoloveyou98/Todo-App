6a1af0d55d8bae3220a32f246783d038cc2e5210
thuctapgit <vinhnt62@wru.vn>
2020-10-22 14:19:51 +0700

    Todo app vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
-
+import routes from './routes';
+import stores from './stores';
 Vue.config.productionTip = false
+Vue.use(VueRouter);
 
+const router = new VueRouter({
+  routes
+})
 new Vue({
+  router,
+  stores,
   render: h => h(App),
 }).$mount('#app')
